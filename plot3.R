714b4810eab5b6794ad470f65de8dac84399ef19
Mark Johnson <mjohnson@HW10424.local>
2016-01-10 15:17:29 -0500

    Update legend handling

--- plot3.R
+++ plot3.R
@@ -7,6 +7,6 @@ with(sampleData, {
   plot(x=recordDate, y=Sub_metering_1, type="l", ylab="Energy sub meeting", xlab="")
   lines(x=recordDate, y=Sub_metering_2, type="l", col="red")
   lines(x=recordDate, y=Sub_metering_3, type="l", col="blue")
-  legend("topright", pch=1, col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+  legend("topright", col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty = c(1,1,1))
 })
 dev.off()
